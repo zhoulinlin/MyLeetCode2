80d53d069e09cdc4d7512328ae0aaabd898e8b11
jolin <441533361@qq.com>
2021-02-01 17:09:56 -0500

    update 221

--- src/_221_MaximalSquare.java
+++ src/_221_MaximalSquare.java
@@ -26,17 +26,75 @@ public class _221_MaximalSquare {
         while(true){
 
             if(x+n >= m.length || y+n >= m[0].length){
-                break;
+                return n;
             }
 
             if(m[x+n][y] == '1' && m[x][y+n] == '1' && m[x+n][y+n] == '1'){
+                for(int j=y+1;j<y+n;j++){
+                    if(m[x+n][j] != '1'){
+                        return n;
+                    }
+                }
+
+                for(int j=x+1;j<x+n;j++){
+                    if(m[j][y+n] != '1'){
+                        return n;
+                    }
+                }
+
                 n++;
             }else{
-                break;
+                return n;
             }
 
         }
-        return n;
+    }
+
+    public int maximalSquare2(char[][] matrix) {
+
+        int cols = matrix[0].length;
+        int rows = matrix.length;
+        int maxsqlen=0;
+        int up,left,dia;
+
+        int [][]dpMatrix = new int[rows][cols];
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+
+                if(matrix[i][j] == '0'){
+                    dpMatrix[i][j] = 0;
+                }else{
+
+                    up = i==0?0:dpMatrix[i - 1][j];
+                    left = j==0?0:dpMatrix[i][j - 1];
+                    dia = (i==0 || j==0)?0:dpMatrix[i - 1][j - 1];
+
+                    dpMatrix[i][j] = Math.min(Math.min(up, left), dia) + 1;
+                    maxsqlen = Math.max(maxsqlen, dpMatrix[i][j]);
+                }
+            }
+
+        }
+        return maxsqlen*maxsqlen;
+    }
+
+
+    public class Solution {
+        public int maximalSquare(char[][] matrix) {
+            int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+            int[][] dp = new int[rows + 1][cols + 1];
+            int maxsqlen = 0;
+            for (int i = 1; i <= rows; i++) {
+                for (int j = 1; j <= cols; j++) {
+                    if (matrix[i-1][j-1] == '1'){
+                        dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                        maxsqlen = Math.max(maxsqlen, dp[i][j]);
+                    }
+                }
+            }
+            return maxsqlen * maxsqlen;
+        }
     }
 
 }
